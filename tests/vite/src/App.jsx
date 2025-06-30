2b60052ad643bff8703504338c368455d1eea397
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-06-30 18:50:18 -0300

    feat: add barebones development app

---
+++ tests/vite/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import { OffscreenWebGL } from '../../../components/index';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Testing / Development environment</h1>
+        <OffscreenWebGL/>
+      </header>
+    </div>
+  )
+}
+
+export default App
