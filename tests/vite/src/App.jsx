953851688858aeb6783ab7630d44cee08847f9e5
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-07-01 15:12:24 -0300

    feat: monitor uniform props

--- tests/vite/src/App.jsx
+++ tests/vite/src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
 import './App.css';
 import { OffscreenWebGL } from '../../../src/index';
+import { useState } from 'react';
+
+function OffscreenWebGLContainer(props) {
+	const [frame, setFrame] = useState(0);
+
+	requestAnimationFrame(() => {
+		setFrame((prev) => prev + 1);
+	});
+
+	return (
+		<div style={{ padding: '20px' }}>
+			<OffscreenWebGL u_resolution={[frame, frame]} />
+		</div>
+	);
+}
 
 function App() {
 	return (
@@ -8,7 +22,7 @@ function App() {
 			<header className="App-header">
 				<h1>Testing / Development environment</h1>
 				<div style={{ width: '350px', height: '200px' }}>
-					<OffscreenWebGL />
+					<OffscreenWebGLContainer />
 				</div>
 			</header>
 		</div>
