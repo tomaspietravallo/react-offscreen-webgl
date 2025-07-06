f5e369e67e4a98a4ff2ed9b46b3a4a4fa7afae61
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-07-06 20:12:25 -0300

    chore: update glsl code to use multiple buffers
    Buffer syntax is from:
    https://github.com/actarian/vscode-glsl-canvas/tree/master
    
    https://marketplace.visualstudio.com/items?itemName=circledev.glsl-canvas

--- tests/vite/public/moving-circle.glsl
+++ tests/vite/public/moving-circle.glsl
@@ -1,12 +1,43 @@
+// Buffer syntax is https://marketplace.visualstudio.com/items?itemName=circledev.glsl-canvas
+
 precision highp float;
+
 uniform vec2 u_resolution;
-uniform float u_frame;
+uniform float u_time;
+
+uniform sampler2D u_buffer0;
+uniform sampler2D u_buffer1;
+
+#if defined(BUFFER_0)
 
 void main() {
 	vec2 uv = gl_FragCoord.xy / u_resolution;
+	vec3 col = texture2D(u_buffer1, uv).rgb;
 	vec2 p = vec2(
-		0.5 + 0.2 * cos(u_frame * 0.1),
-		0.5 + 0.2 * sin(u_frame * 0.1)
+		0.5 + 0.2 * cos(u_time * 1.0),
+		0.5 + 0.2 * sin(u_time * 1.0)
 	);
-	gl_FragColor = vec4(vec3(1.0 - smoothstep(0.0, 0.1, length(uv - p))), 1.);
+	gl_FragColor = vec4( col * 0.98 + vec3(1.0 - smoothstep(0.0, 0.05, length(uv - p))), 1.);
 }
+
+#elif defined(BUFFER_1)
+
+void main() {
+	vec2 uv = gl_FragCoord.xy / u_resolution;
+	vec3 col = texture2D(u_buffer0, uv).rgb;
+	vec2 p = vec2(
+		0.5 + 0.2 * cos(u_time * 1.0),
+		0.5 + 0.2 * sin(u_time * 1.0)
+	);
+	gl_FragColor = vec4( col * 0.98 + vec3(1.0 - smoothstep(0.0, 0.05, length(uv - p))), 1.);
+}
+
+#else
+
+void main() {
+	vec2 uv = gl_FragCoord.xy / u_resolution;
+	vec3 col = texture2D(u_buffer1, uv).rgb;
+	gl_FragColor = vec4(col, 1.);
+}
+
+#endif
